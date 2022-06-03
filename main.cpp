7fa4568400cf14bda976418e0a98d959c8a93345
tcarlucc <thomascarlucci@me.com>
2022-06-02 19:45:03 -0700

    Started Card.h, might implement deck as seperate class in the future.

---
+++ main.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file main.cpp
+ * @author Thomas Carlucci (thomascarlucci@me.com)
+ * @brief Monte Carlo simulation on the odds for winning a blackjack hand with 3 hits
+ * @version 0.1
+ * @date 2022-06-01
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Card.h"
+
+
+int main() {
+    
+
+    return 0;
+}
+
+/**
+ * @brief Simulates a game of blackjack with a given number of players
+ * 
+ * @param numPlayers integer value from 1-4
+ * @return int: 0 on loss, 1 on win
+ */
+int simulateHand(int numPlayers) {
+
+    return 0;
+}
